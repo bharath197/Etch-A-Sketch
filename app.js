1d9f3eb57a18d93f9269c11a272b9dd031ae5917
bharath197 <125417055+bharath197@users.noreply.github.com>
2023-04-10 21:09:27 +0530

    add button

--- app.js
+++ app.js
@@ -1,24 +1,48 @@
 const body = document.querySelector('body')
+const btn = document.createElement('button')
+btn.textContent = 'click me'
 const containerDiv = document.createElement('div')
 containerDiv.classList.add('container')
+body.appendChild(btn)
 body.appendChild(containerDiv)
 
-for(let i = 0; i<256; i++){
-    const div = document.createElement('div')
-    let className = `innerBox`
-    div.classList.add(className)
-    let numberOfSquares = Math.sqrt(256);
-    let width = 960/numberOfSquares
-    console.log(width)
-    div.style.width = `${width}px`
-    containerDiv.appendChild(div)
-}
-const divs = document.querySelectorAll('.innerBox')
 
-divs.forEach(div=>div.addEventListener('mouseover',()=>{
-    let color = `rgb(${randomnumber(256)},${randomnumber(256)},${randomnumber(256)})`
-    div.style.backgroundColor= color
-}))
+btn.addEventListener('click', ()=>{
+    const value = prompt('Enter a number')
+    totalBox = value*value
+    if(value>100){
+        alert('Number too high')
+        return
+    }
+    removeBox(containerDiv)
+    for(let i = 0; i<totalBox; i++){
+        const div = document.createElement('div')
+        let className = `innerBox`
+        div.classList.add(className)
+        
+        let width = 960/value
+        div.style.width = `${width}px`
+
+        containerDiv.appendChild(div)
+    }
+    etchASketch();
+})
+
+function removeBox(div){
+    while(div.firstChild){
+        div.removeChild(div.firstChild)
+    }
+}
+function etchASketch(){
+    const divs = document.querySelectorAll('div.innerBox')
+    console.log(divs)
+    
+    divs.forEach(div=>div.addEventListener('mouseover',()=>{
+        let color = `rgb(${randomnumber(256)},${randomnumber(256)},${randomnumber(256)})`
+        div.style.backgroundColor= color
+    }))
+}
+
 function randomnumber(n){
     return Math.floor(Math.random()*n)+1
 }
